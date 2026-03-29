7e34135e80769cc56ae19ed3b72c9de01cfb06cd
aayushbtw <21987529+aayushbtw@users.noreply.github.com>
2026-03-29 14:02:30 +0530

    feat(zsh): add la and ld eza aliases

--- config/zsh/aliases.zsh
+++ config/zsh/aliases.zsh
@@ -3,7 +3,9 @@ alias o='open .'
 alias genpass='openssl rand -hex 32'
 
 if has_or_warn eza; then
-  alias ls='eza -l -g --icons'
+  alias ls='eza -l --icons'
+  alias la='eza -l -a --icons'
+  alias ld='eza -l -D --icons'
   alias ll='eza --icons'
 fi
 
